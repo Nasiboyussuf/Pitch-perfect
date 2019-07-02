972f5ca6187ba8b72e1c476a3a4bf7a8d8e78b69
Nasibo Yussuf <yussufnasibo@gmail.com>
2019-07-02 10:50:27 +0300

    change folder structure due  to errors

---
+++ config.py
@@ -0,0 +1,20 @@
+
+import os
+
+class Config:
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitch'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
