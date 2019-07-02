e4769a6015447fc623981157c10d21f810ec4163
Nasibo Yussuf <yussufnasibo@gmail.com>
2019-07-02 11:52:35 +0300

    change template structure

--- app/models.py
+++ app/models.py
@@ -9,3 +9,15 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+        
+
+class pitch(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'pitch',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
