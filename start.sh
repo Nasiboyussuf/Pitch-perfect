35e060f81c6903ff7093560cfe77e088013b232b
Nasibo Yussuf <yussufnasibo@gmail.com>
2019-07-04 10:25:45 +0300

    add mail username and password

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
-from app import app
+export SECRET_KEY='4547'
+SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:12345@localhost/pitch'
+export MAIL_USERNAME='yussufnasibo@gmail.com'
+export MAIL_PASSWORD='0799332404'
 
-if __name__ == '__main__':
-    app.run(debug = True)
+
+python3.6 manage.py server
