733f277f2e3b121b68ed236fdc1ba253cb5e1136
Nasibo Yussuf <yussufnasibo@gmail.com>
2019-07-04 13:50:09 +0300

    add all the configurations

--- config.py
+++ config.py
@@ -3,15 +3,27 @@ import os
 
 class Config:
     
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitch'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    UPLOADS_DEFAULT_DEST ='app/static'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 588
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+            
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+    
 
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitch'
     DEBUG = True
 
 config_options = {
